b7beba85305fa930c94ef142f722ddba747c339b
thekevin1 <140349111+thekevin1@users.noreply.github.com>
2024-01-22 17:03:54 -0500

    Update Color.php

--- src/zkevinezzk/armorcolor/Color.php
+++ src/zkevinezzk/armorcolor/Color.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace zkevinezzk\armorcolor;
+
+use zkevinezzk\armorcolor\command\ColorCommand;
+
+use pocketmine\plugin\PluginBase;
+
+class Color extends PluginBase {
+
+    public function onEnable(): void {
+
+        $this->getServer()->getCommandMap()->register("color", new ColorCommand($this));
+
+        $this->getLogger()->info("
+░█████╗░██████╗░███╗░░░███╗░█████╗░██████╗░  ░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░
+██╔══██╗██╔══██╗████╗░████║██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗
+███████║██████╔╝██╔████╔██║██║░░██║██████╔╝  ██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝
+██╔══██║██╔══██╗██║╚██╔╝██║██║░░██║██╔══██╗  ██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗
+██║░░██║██║░░██║██║░╚═╝░██║╚█████╔╝██║░░██║  ╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝  ░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝ Plugin by zkevinezzk.");
+    }
+
+    public function onDisable(): void {
+        $this->getLogger()->info("Armor Color disabled ");
+    }
+}
